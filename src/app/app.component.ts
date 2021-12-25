8b1a966739b72c28ea62eaf9b59ca9851c54a646
Vlad <vladdaking@gmail.com>
2021-12-25 22:48:51 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'FCA';
+}
